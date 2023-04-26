201b345266407f701449dc805984aad71bf237f4
jeanduarte00 <jean_luccas320@hotmail.com>
2023-04-25 22:12:18 -0300

    added more opperations

--- math.go
+++ math.go
@@ -1,11 +1,17 @@
 package main
 
-import "fmt"
-
-func main() {
-	fmt.Println(soma(112, 10))
+func sumWith(a int, b int) int {
+	return a + b
 }
 
-func soma(a int, b int) int {
-	return a + b
+func subtractBy(a int, b int) int {
+	return a - b
+}
+
+func divideBy(a int, b int) int {
+	return a / b
+}
+
+func multipleBy(a int, b int) int {
+	return a * b
 }
