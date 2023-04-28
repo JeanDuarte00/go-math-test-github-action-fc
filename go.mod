10643dfae027606a1ffebbbc19c6da30a7578fe4
jeanduarte00 <jean_luccas320@hotmail.com>
2023-04-28 17:36:30 -0300

    feat: add new jobs and refactoring

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module math
+module mathematics
 
-go 1.19
+go 1.13
--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-	calc "math/calculator"
+	calc "mathematics/calculator"
 )
 
 func main() {
+	fmt.Println("Hello, World!")
 	fmt.Println(calc.SumWith(112, 10))
 	fmt.Println(calc.SubtractBy(112, 10))
 	fmt.Println(calc.MultipleBy(112, 10))
